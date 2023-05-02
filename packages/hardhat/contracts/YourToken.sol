1137b902978bcca4b88994552e77f5633d3770cb
allannnc <allan@gmistudios.com.au>
2023-05-02 12:51:12 +1000

    add todo

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.8.19; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// @ALLAN: fix error here (workaround ../node_modules/[samePath])
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 // @TODO: Implement the YourToken smart contract
