7219bcc4f5a80e9f2d592bed13639e388286cb39
allannnc <allan@gmistudios.com.au>
2023-05-03 17:53:54 +1000

    Comment out most of ContractInteraction

--- packages/nextjs/components/example-ui/ContractInteraction/index.tsx
+++ packages/nextjs/components/example-ui/ContractInteraction/index.tsx
@@ -2,38 +2,38 @@ import { useState } from "react";
 import { CopyIcon } from "../assets/CopyIcon";
 import { DiamondIcon } from "../assets/DiamondIcon";
 import { HareIcon } from "../assets/HareIcon";
-import { ContractInteractionCard } from "./ContractInteractionCard";
-import { ethers } from "ethers";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-// import eth price hook here
-import { useDeployedContractInfo, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
+// import { ContractInteractionCard } from "./ContractInteractionCard";
+// import { ethers } from "ethers";
+// import { useDeployedContractInfo, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const ContractInteraction = () => {
-  const vendor = useDeployedContractInfo("Vendor");
+  // const vendor = useDeployedContractInfo("Vendor");
 
-  const [visible, setVisible] = useState(true);
+  const [areInstructionsVisible, setAreInstructionsVisible] = useState(true);
   // @TODO: change this to be the number of `YourToken`s, not ETH
-  const [buyAmount, setBuyAmount] = useState("");
-  const [approveAmount, setApproveAmount] = useState("");
-  const [sellAmount, setSellAmount] = useState("");
+  // const [buyAmount, setBuyAmount] = useState("");
+  // const [approveAmount, setApproveAmount] = useState("");
+  // const [sellAmount, setSellAmount] = useState("");
 
-  const { writeAsync: buyTokensAsync, isLoading: buyTokensIsLoading } = useScaffoldContractWrite({
-    contractName: "Vendor",
-    functionName: "buyTokens",
-    value: buyAmount,
-  });
+  // const { writeAsync: buyTokensAsync, isLoading: buyTokensIsLoading } = useScaffoldContractWrite({
+  //   contractName: "Vendor",
+  //   functionName: "buyTokens",
+  //   value: buyAmount,
+  // });
 
-  const { writeAsync: approveAsync, isLoading: approveIsLoading } = useScaffoldContractWrite({
-    contractName: "YourToken",
-    functionName: "approve",
-    args: [vendor.data?.address, ethers.utils.parseEther(approveAmount || "0")],
-  });
+  // const { writeAsync: approveAsync, isLoading: approveIsLoading } = useScaffoldContractWrite({
+  //   contractName: "YourToken",
+  //   functionName: "approve",
+  //   args: [vendor.data?.address, ethers.utils.parseEther(approveAmount || "0")],
+  // });
 
-  const { writeAsync: sellTokensAsync, isLoading: sellIsLoading } = useScaffoldContractWrite({
-    contractName: "Vendor",
-    functionName: "sellTokens",
-    args: [ethers.utils.parseEther(sellAmount || "1")],
-  });
+  // const { writeAsync: sellTokensAsync, isLoading: sellIsLoading } = useScaffoldContractWrite({
+  //   contractName: "Vendor",
+  //   functionName: "sellTokens",
+  //   args: [ethers.utils.parseEther(sellAmount || "1")],
+  // });
 
   return (
     <div className="flex bg-base-300 relative pb-10">
@@ -42,7 +42,7 @@ export const ContractInteraction = () => {
       <HareIcon className="absolute right-0 bottom-24" />
 
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
-        <div className={`mt-10 flex gap-2 ${visible ? "" : "invisible"} max-w-2xl`}>
+        <div className={`mt-10 flex gap-2 ${areInstructionsVisible ? "" : "invisible"} max-w-2xl`}>
           <div className="flex gap-5 bg-base-200 bg-opacity-80 z-0 p-7 rounded-2xl shadow-lg">
             <span className="text-3xl">👋🏻</span>
             <div>
@@ -61,13 +61,13 @@ export const ContractInteraction = () => {
           </div>
           <button
             className="btn btn-circle btn-ghost h-6 w-6 bg-base-200 bg-opacity-80 z-0 min-h-0 drop-shadow-md"
-            onClick={() => setVisible(false)}
+            onClick={() => setAreInstructionsVisible(false)}
           >
             <XMarkIcon className="h-4 w-4" />
           </button>
         </div>
 
-        <ContractInteractionCard
+        {/* <ContractInteractionCard
           label="Buy Tokens"
           setValue={setBuyAmount}
           onClick={buyTokensAsync}
@@ -75,25 +75,25 @@ export const ContractInteraction = () => {
           subtitleLabel="You pay"
           // @TODO: make sure to update this too!
           subtitleValue={`${buyAmount} ETH + Gas`}
-        />
+        /> */}
 
-        <ContractInteractionCard
+        {/* <ContractInteractionCard
           label="Approve"
           setValue={setApproveAmount}
           onClick={approveAsync}
           buttonIsLoading={approveIsLoading}
           subtitleLabel="You pay"
           subtitleValue="Gas"
-        />
+        /> */}
 
-        <ContractInteractionCard
+        {/* <ContractInteractionCard
           label="Sell Tokens"
           setValue={setSellAmount}
           onClick={sellTokensAsync}
           buttonIsLoading={sellIsLoading}
           subtitleLabel="You receive"
           subtitleValue={`${ethers.utils.formatEther(ethers.utils.parseEther(sellAmount || "0").div(100))} ETH - Gas`}
-        />
+        /> */}
       </div>
     </div>
   );
