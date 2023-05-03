73ab9f00d2a5f9160f028591ba308bf1476633cf
allannnc <allan@gmistudios.com.au>
2023-05-03 17:22:54 +1000

    add link to ownable docs

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.8.19; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
+// learn more: https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
 // import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
